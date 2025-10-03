ddf2eca651899f07e16c0bd605e78e25d781d362
Tyrnpuu <reivotyrp@gmail.com>
2025-10-03 16:58:09 +0300

    C. Added verbose flag

--- main.py
+++ main.py
@@ -6,24 +6,33 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
-
-
 
 def main():
     if len(sys.argv) < 2:
         print("Error: No prompt provided.")
         sys.exit(1)
-    prompt = sys.argv
+
+    user_prompt = sys.argv[1]
+    verbose = '--verbose' in sys.argv
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
     response = client.models.generate_content(
-        model="gemini-2.0-flash", 
-        contents=prompt[1:])
+        model="gemini-2.0-flash-001", 
+        contents=messages)
     
     print("Hello from code-ai!")
     print(response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 if __name__ == "__main__":
     main()
